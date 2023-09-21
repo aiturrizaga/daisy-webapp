3d161361add2c0473f76724c4a4f83e83dc28167
Alvaro Iturrizaga <aiturrizaga@vallegrande.edu.pe>
2023-09-21 14:18:24 -0500

    Flower page finished

--- src/app/flower/flower.component.ts
+++ src/app/flower/flower.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-flower',
   templateUrl: './flower.component.html',
   styleUrls: ['./flower.component.scss']
 })
-export class FlowerComponent {
+export class FlowerComponent implements AfterViewInit {
 
+  audioPath = 'assets/sounds/flores-amarillas-song_short.mp3';
+  // @ts-ignore
+  @ViewChild('audio') audio: ElementRef;
+  // @ts-ignore
+  @ViewChild('button') btn: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.audio.nativeElement.play();
+    this.btn.nativeElement.click();
+    this.audio.nativeElement.play();
+  }
 }
